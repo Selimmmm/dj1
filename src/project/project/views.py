8514e708150f9eac9267a78656abdf8b489aac3f
Selim <selim.mellouk@essec.edu>
2023-05-30 12:07:29 +0200

    init commit

---
+++ src/project/project/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def squares(request):
+    x = []
+    for i in range(10):
+        x.append(str(i**2))
+    return HttpResponse(f"<p>{'<br>'.join(x)}</p>")
