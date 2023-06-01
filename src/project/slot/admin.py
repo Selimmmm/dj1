9f2c0f60d655c418089e20b1a6189971bceb37ef
Selim <selim.mellouk@essec.edu>
2023-06-01 14:43:55 +0200

    model TimeSlot Created + Admin

--- src/project/slot/admin.py
+++ src/project/slot/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TimeSlot
+
+
+class TimeSlotAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "slot_type",
+        "date_start",
+        "date_end",
+        "date_created",
+    )
+
+
+admin.site.register(TimeSlot, TimeSlotAdmin)
--- src/project/slot/models.py
+++ src/project/slot/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.db.models import Model
 
-# Create your models here.
+
+class TimeSlot(Model):
+    JEAN = "JEAN"
+    JEANNE = "JEANNE"
+    JOHN = "JOHN"
+
+    PEOPLE = [
+        (JEAN, "JEAN"),
+        (JEANNE, "JEANNE"),
+        (JOHN, "JOHN"),
+    ]
+
+    first_name = models.CharField(
+        choices=PEOPLE, blank=False, null=False, max_length=255
+    )
+
+    slot_type = models.CharField(blank=False, null=False, max_length=255)
+    message = models.TextField(blank=False, null=False)
+    date_start = models.DateTimeField(blank=False, null=False)
+    date_end = models.DateTimeField(blank=False, null=False)
+    date_created = models.DateTimeField(auto_now_add=True)
