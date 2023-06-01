18968d78b9de93b617a78c9341d77e4a587b787b
Selim <selim.mellouk@essec.edu>
2023-06-01 15:02:23 +0200

    my form is so beautiful i'll go full frontend

---
+++ src/project/slot/form.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import TimeSlot
+
+import datetime
+
+
+ATTRS_FIELD = {"class": "input mb-4"}
+
+
+class DateInput(forms.DateTimeInput):
+    input_type = "datetime-local"
+
+
+class TimeSlotForm(forms.Form):
+    first_name = forms.ChoiceField(
+        choices=TimeSlot.PEOPLE, widget=forms.Select(attrs=ATTRS_FIELD)
+    )
+    slot_type = forms.CharField(widget=forms.TextInput(attrs=ATTRS_FIELD))
+    message = forms.CharField(required=False, widget=forms.Textarea(attrs=ATTRS_FIELD))
+    date_start = forms.DateTimeField(
+        label="Starting date",
+        widget=DateInput(attrs=ATTRS_FIELD),
+    )
+
+    date_end = forms.DateTimeField(
+        label="Starting date",
+        widget=DateInput(attrs=ATTRS_FIELD),
+    )
+
+    date_created = forms.DateTimeField(
+        label="Starting date",
+        widget=DateInput(attrs={"class": "input"}),
+    )
+
+    class Meta:
+        model = TimeSlot
+        # fields = "_all_" # pas nécessaire si tous spécifiés au dessus
--- src/project/slot/views.py
+++ src/project/slot/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 
 
+from .form import TimeSlotForm
+
+
 @require_http_methods(["GET", "POST"])
 def give_time_slots(request):
     if request.method == "POST":
         pass
-    context = {}
+
+    form = TimeSlotForm()
+    context = {"form": form}
     return render(request, "slots/give_time_slots.html", context=context)
